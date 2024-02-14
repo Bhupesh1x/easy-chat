eeaf24c9df8ef34fa149dca5d70d5f0fb2c44a6c
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-02-14 18:17:36 +0530

    Created a different route group for site.

---
+++ app/(site)/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+import AuthForm from "./_components/AuthForm";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col h-full items-center justify-center">
+      <div className="mx-auto">
+        <Image
+          src="/images/logo.png"
+          alt="logo"
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+        />
+        <h1 className="mt-6 text-3xl text-gray-800 font-bold tracking-tight text-center">
+          Sign in to your account
+        </h1>
+      </div>
+      <AuthForm />
+    </div>
+  );
+}
