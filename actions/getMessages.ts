d8fc500938462eb0aed4d1c7106eb560c33f95a8
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-03-05 23:22:54 +0530

    Created backend server action for getting the messages for a particular conversation.

---
+++ actions/getMessages.ts
@@ -0,0 +1,24 @@
+import db from "@/libs/db";
+
+async function getMessages(conversationId: string) {
+  try {
+    const messages = await db.message.findMany({
+      where: {
+        conversationId: conversationId,
+      },
+      include: {
+        seen: true,
+        sender: true,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return messages;
+  } catch (error) {
+    return [];
+  }
+}
+
+export default getMessages;
