b195c3cc564fbb2fc9673125e5dd089c4bc9ab00
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-03-06 20:13:49 +0530

    Backend: Created route for creating messages.

---
+++ app/api/messages/route.ts
@@ -0,0 +1,67 @@
+import db from "@/libs/db";
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/actions/getCurrentUser";
+
+export async function POST(req: Request) {
+  try {
+    const curretUser = await getCurrentUser();
+    const { message, image, conversationId } = await req.json();
+
+    if (!curretUser?.id || !curretUser?.email) {
+      return new NextResponse("User unauthorized", { status: 401 });
+    }
+
+    const newMessage = await db.message.create({
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: {
+            id: conversationId,
+          },
+        },
+        sender: {
+          connect: {
+            id: curretUser.id,
+          },
+        },
+        seen: {
+          connect: {
+            id: curretUser?.id,
+          },
+        },
+      },
+      include: {
+        seen: true,
+        sender: true,
+      },
+    });
+
+    const updatedConversation = await db.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(newMessage);
+  } catch (error) {
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
